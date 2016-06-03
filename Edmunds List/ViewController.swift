a822cc06582ef2deb3fd78a2ff9d6d21b7e336ea
Kamal Dandamudi <kamal.dandamudi@vensi.com>
2016-06-03 12:42:57 -0500

    Initial Commit

---
+++ Edmunds List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Edmunds List
+//
+//  Created by Kamal Dandamudi on 6/3/16.
+//  Copyright © 2016 Kamal Dandamudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
